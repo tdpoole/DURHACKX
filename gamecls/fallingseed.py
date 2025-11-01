2bc9d4e97d2f95c4d5f66430b5b30062e5d2b0a0
Thomas Poole <105967819+tdpoole@users.noreply.github.com>
2025-11-01 15:40:07 +0000

    Fixed rendering

--- gamecls/fallingseed.py
+++ gamecls/fallingseed.py
@@ -4,24 +4,14 @@ from random import randint
 from worldobject import WorldObject
 
 class FallingSeed(WorldObject):
-    def __init__(self, x, y, assets: AssetManager, parentTree, index):
+    def __init__(self, x, y, assets: AssetManager):
         self.rect = pygame.Rect(x,y,10,10)
         self.attachedToTree = True
         super().__init__(x, y, assets.seed)
-        self.parent = parentTree
-        self.index = index
 
     def update(self, game):
         if not self.attachedToTree:
             self.rect.y += 1
-
         if self.attachedToTree:
-            if randint(0,1000)==0:
+            if randint(0,100)==0:
                 self.attachedToTree = False
-
-    def draw(self, screen):
-        screen.blit(self.surf, self.rect)
-
-    def plant(self, game):
-        game.trees.append(Tree(self.rect.x,self.rext.y,self.assets))
-        self.parent.seeds.pop(self.index)
