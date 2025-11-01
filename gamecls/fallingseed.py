46162ec995475324c2e4c700598c99f6ba613262
Axel Tettenborn <ldwl31@durham.ac.uk>
2025-11-01 15:13:23 +0000

    seed turning into tree

--- gamecls/fallingseed.py
+++ gamecls/fallingseed.py
@@ -1,20 +1,28 @@
 import pygame
 from assetmanager import AssetManager
 from random import randint
+from settings import *
+from main import *
 
 class FallingSeed:
     def __init__(self, x, y, assets:AssetManager):
         self.rect = pygame.Rect(x,y,10,10)
         self.surf = assets.seed
+        self.assets = assets
         self.attachedToTree = True
 
-    def update(self):
+    def update(self, game):
         if not self.attachedToTree:
-            self.rect.y += 1
-
-        if self.attachedToTree:
+            self.rect.y += 3
+            self.rect.x += randint(-1,1)
+            if self.rect.y == GROUND_Y_LEVEL:
+                self.plant(game)
+        else:
             if randint(0,1000)==0:
                 self.attachedToTree = False
 
     def draw(self, screen):
         screen.blit(self.surf, self.rect)
+
+    def plant(self, game):
+        game.trees.append(Tree(self.rect.x,self.rext.y,self.assets))
