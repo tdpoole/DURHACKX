aff70020b3c98a28c577d589880164f7ee4d7f8e
Thomas Poole <105967819+tdpoole@users.noreply.github.com>
2025-11-01 13:09:38 +0000

    Added extra growth stages to assets

--- assetmanager.py
+++ assetmanager.py
@@ -5,6 +5,10 @@ class AssetManager:
         self.seed = pygame.image.load('assets/placeholder.svg')
 
         self.treeGrowth0 = pygame.image.load('assets/placeholder.svg')
+        self.treeGrowth1 = pygame.image.load('assets/placeholder.svg')
+        self.treeGrowth2 = pygame.image.load('assets/placeholder.svg')
+        self.treeGrowth3 = pygame.image.load('assets/placeholder.svg')
+        self.treeGrowth4 = pygame.image.load('assets/placeholder.svg')
 
         self.fulltreeGrowth = pygame.image.load('assets/Images/FullTree.png')
 
