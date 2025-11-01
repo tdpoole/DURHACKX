0e2d0a4bca0a47ceae179ab5c259630b8c0a7c83
Thomas Poole <105967819+tdpoole@users.noreply.github.com>
2025-11-01 13:20:04 +0000

    Implemented correct scaling in assetmanager

--- assetmanager.py
+++ assetmanager.py
@@ -1,16 +1,20 @@
 import pygame
+from settings import PIXEL_SCALE_FACTOR
+
+def import_image(imgpath):
+    return pygame.transform.scale_by(pygame.image.load(imgpath), PIXEL_SCALE_FACTOR)
 
 class AssetManager:
     def __init__(self):
-        self.seed = pygame.image.load('assets/placeholder.svg')
+        self.seed = import_image('assets/placeholder.svg')
 
-        self.treeGrowth0 = pygame.image.load('assets/placeholder.svg')
-        self.treeGrowth1 = pygame.image.load('assets/placeholder.svg')
-        self.treeGrowth2 = pygame.image.load('assets/placeholder.svg')
-        self.treeGrowth3 = pygame.image.load('assets/placeholder.svg')
-        self.treeGrowth4 = pygame.image.load('assets/placeholder.svg')
+        self.treeGrowth1 = import_image('assets/placeholder.svg')
+        self.treeGrowth2 = import_image('assets/placeholder.svg')
+        self.treeGrowth0 = import_image('assets/placeholder.svg')
+        self.treeGrowth3 = import_image('assets/placeholder.svg')
+        self.treeGrowth4 = import_image('assets/placeholder.svg')
 
-        self.fulltreeGrowth = pygame.image.load('assets/Images/FullTree.png')
+        self.fulltreeGrowth = import_image('assets/Images/FullTree.png')
 
-        self.groundBackground = pygame.image.load('assets/Images/GroundBackground.png')
-        self.skyBackground = pygame.image.load('assets/Images/SkyBackground.png')
+        self.groundBackground = import_image('assets/Images/GroundBackground.png')
+        self.skyBackground = import_image('assets/Images/SkyBackground.png')
--- gamecls/tree.py
+++ gamecls/tree.py
@@ -1,11 +1,12 @@
 import pygame
 import assetmanager
+from settings import *
 
 class Tree:
     def __init__(self, x, y, assets:assetmanager.AssetManager):
 
         self.rect = pygame.rect.Rect(x,y,100,100)
-
+        
         self.growthStage = 0
 
         self.growthSurfs = []
