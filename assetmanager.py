75725ed5a08b5c112ba5a0c7099f3d733592d474
Thomas Poole <105967819+tdpoole@users.noreply.github.com>
2025-11-01 13:00:53 +0000

    Added images to assetmanager

--- assetmanager.py
+++ assetmanager.py
@@ -5,3 +5,8 @@ class AssetManager:
         self.seed = pygame.image.load('assets/placeholder.png')
 
         self.treeGrowth0 = pygame.image.load('assets/placeholder.png')
+
+        self.fulltreeGrowth = pygame.image.load('assets/Images/FullTree.png')
+
+        self.groundBackground = pygame.image.load('assets/Images/GroundBackground.png')
+        self.skyBackground = pygame.image.load('assets/Images/SkyBackground.png')
