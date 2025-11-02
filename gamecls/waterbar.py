9672cce54acb0838d153d6134067a1fc52807b3a
Thomas Poole <105967819+tdpoole@users.noreply.github.com>
2025-11-02 01:00:48 +0000

    Added waterbar

---
+++ gamecls/waterbar.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+class WaterBar:
+    def __init__(self, x, y, width, height):
+        self.bgRect = pygame.Rect(x, y, width, height)
+        self.fgRect = pygame.Rect(x, y, width, height)
+    def update(self, game):
+        fillamount = game.menuBar.watertank./game.menuBar.watertank.sizes[game.menuBar.watertank.stage]
+        self.fgRect.width = self.bgRect.width*fillamount
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, pygame.Color(255,0,0), self.bgRect)
+        pygame.draw.rect(screen, pygame.Color(255,0,0), self.fgRect)
