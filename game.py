44c3f800087c2f53a1ea8e858118da8cae32b033
Axel Tettenborn <ldwl31@durham.ac.uk>
2025-11-02 00:44:06 +0000

    added buying next watertank

--- game.py
+++ game.py
@@ -42,7 +42,7 @@ class Game:
         self.precipitation.update(self)
         self.selected = self.menuBar.axe.update(player_input, self.selected)
         self.selected = self.menuBar.wateringcan.update(player_input, self.selected)
-        self.menuBar.watertank.update(player_input)
+        self.currency.amount = self.menuBar.watertank.update(player_input, self.currency.amount)
 
         if randint(0,1000) == 0:
             self.menuBar.gwValue += 1
